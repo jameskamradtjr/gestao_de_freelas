e4564592728005160ab3fd5891b2505dde318bc2
James Kamradt Jr <jameskamradtjr@gmail.com>
2018-05-16 17:15:03 -0300

    Primeiro Commit

---
+++ application/logs/log-2018-05-16.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-16 22:07:04 --> Severity: Warning  --> Declaration of ActiveRecord\DateTime::setTime($hour, $minute, $second = NULL) should be compatible with DateTime::setTime($hour, $minute, $second = NULL, $microseconds = NULL) C:\xampp\htdocs\gestao_freela\sparks\php-activerecord\0.0.3\vendor\php-activerecord\lib\DateTime.php 150
